42ac3585045469aae3d25c221b31fafa46d190db
Zuglya <irussel86@gmail.com>
2024-09-19 00:43:18 +0400

    Добавлен Lesson4.1

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+//        int cat = 5;
+//        int car = 10;
+//        int catCount = 15;
+//        int carCount = 20;
+//        int catInCar = 25;
+//        String text = "Lalapalooza";
+//        char ch1 = 75;
+//        char ch2 = 43;
+//        double coord = 32.234;
+//        System.out.println(cat);
+//        System.out.println(car);
+//        System.out.println(catCount);
+//        System.out.println(carCount);
+//        System.out.println(catInCar);
+//        System.out.println(text);
+//        System.out.println(ch1);
+//        System.out.println(ch2);
+//        System.out.println(coord);
+//        int a = 132;
+//        int b = 32;
+//        System.out.println(a + " + " + b + " = " + (a + b));
+//        System.out.println(a + " - " + b + " = " + (a - b));
+//        System.out.println(a + " / " + b + " = " + (a / b));
+        double a = 132.6;
+        double b = 32.2;
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " / " + b + " = " + (a / b));
+
+    }
+}
